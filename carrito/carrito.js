5e830283ecb02965e4ec7b67af9e906952ad8706
Ariel3748 <ariel.oliva3748@gmail.com>
2024-12-12 00:30:40 -0300

    Prueba carrito
    Pruebo funcionalidades de carrito en un archivo aparte al proyecto

--- carrito/carrito.js
+++ carrito/carrito.js
@@ -39,22 +39,66 @@ const productos = [
 ];
 
 function mostrarProductos(productosAmostrar){
-    let html = "";
-    productosAmostrar.forEach(producto => {
-        html += `
+    let html = "";              //Creo una variable vacia html que "borra" las cosas que tenga el html
+    productosAmostrar.forEach(producto => {         //Recorro el JSON o el array del paramtero 
+        html += `                                   
         <article data-id="${producto.id}">
             <h2>${producto.nombre}</h2>
             <p>${producto.descripicion}</p>
             <p>${producto.precio}</p>
+            <button type="button" class="Agregar-item">Agregar Item</button>
         </article>
-        `
+        `                           //para cada elemento del JSON creo un article con los datos del JSON y con += los va concatenando para que no se reemplcen
         
     });
 
-    let containerProductos = document.querySelector("#container_productos")
-    containerProductos.innerHTML = html;
+    let containerProductos = document.querySelector("#container_productos") 
+    containerProductos.innerHTML = html; //Agrego la variable html con los datos del JSON a la pagina
 
 }
 
 //Cuando carga la pagina se ejecuta la funcion que agarra el JSON y muestra los productos
 document.addEventListener("DOMContentLoaded",mostrarProductos(productos));
+
+
+//Obtengo Solamente el id, nombre y precio de el producto
+function filtroDatosProducto(productoRecibido){
+    const {id, nombre, precio} = productoRecibido
+    const producto = {
+        id: id,
+        nombre : nombre,
+        precio: precio,
+    } 
+    return(producto)
+}
+
+
+// Declaro un carrito nuevo o en caso de que ya haya uno lo levanto
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+
+document.addEventListener("click", (event) => {             //Escucha el evento click en todo el documento
+    if(event.target.classList.contains("Agregar-item")){    //Si a lo que hago click [(event es el click) y target es a que le hago click] contiene la clase "Agregar-item"
+        const idConsulta = event.target.closest("article").dataset.id;  //Crea la variable id consulta y guarda el dataset con el tag id del elemento padre de donde fue efectuado el click
+        const elemento = Object.values(productos).find(producto => producto.id === Number(idConsulta)); /*
+        Transformo productos a un objeto array con (Object) y .values devuelve el valor de Productos; en este array busco con find() la cioncidencia 
+        de producto.id y id consulta (que hay que pasarlo a numero porque el dom en el html lo tiene como string) y cuando encuentra la coincidencia
+        devuelve el objeto producto
+        */
+       const producto = filtroDatosProducto(elemento)
+       console.log(producto)
+       console.log(carrito)
+
+       //Agrego el producto al carrito
+       carrito.push(producto)
+
+       //Guardo el carrito en formato JSON dentro del local storage
+        localStorage.setItem("carrito",JSON.stringify(carrito));
+    }
+})
+
+function limpiarCarrito(carrito){
+    carrito.length = 0
+}
+
