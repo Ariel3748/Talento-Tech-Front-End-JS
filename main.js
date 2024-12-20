3fe74b7fe8c83663613e6605a58ac2038a22def5
Ariel3748 <ariel.oliva3748@gmail.com>
2024-12-20 02:20:16 -0300

    Agrego carrito
    Agrego la pagina del carrito y la funcionalidad de las cantidades respecto a los productos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function mostrarProductos(productosAmostrar){
     var html = "";
     productosAmostrar.forEach(producto => {
         html += `
-        <article>
+        <article data-id="${producto.id}">
         <img
           src="${producto.imagen}"
           alt="${producto.marca} ${producto.modelo}"
@@ -12,25 +12,122 @@ function mostrarProductos(productosAmostrar){
           <li>Año: ${producto.año}</li>
           <li>Km: ${producto.kilometraje}</li>
           <li>Estado: ${producto.estado}</li>
+          <li>Precio: ${producto.precio}$USD</li> 
         </ul>
-        <a href="">
           <button class="pointer" >Ver mas..</button>
-        </a>
+          <button class="Agregar_Item" >Agregar al carrito</button>
       </article>        
         `
     })
 
-    let productos = document.querySelector("#productos") 
-    productos.innerHTML = html;
+    const productos = document.querySelector("#productos") 
+    productos.innerHTML += html;
 }
 
 function traerDatosJSON(json){
     fetch(json)
         .then(respuesta => (respuesta.json()))
         .then((datos)=> (mostrarProductos(datos)))
+}
+
+
+function filtroDatosProducto(producto){
+    const {id, marca, modelo, precio} = producto
+    var productoFiltrado = {
+      id: id,
+      marca: marca,
+      modelo: modelo,
+      precio: precio,
+      cantidad : 1,
+      
+    }
+
+  return(productoFiltrado)
+}
+
+document.addEventListener("DOMContentLoaded",traerDatosJSON("autos/Autos.txt"));
+
+
+// Declaro un carrito nuevo o en caso de que ya haya uno lo levanto
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+function agregarAlCarrito(elementoAagregar){
+
+  const productoExistente = carrito.find(auto => auto.id == elementoAagregar.id);
+
+  if (productoExistente) {
+      productoExistente.cantidad += elementoAagregar.cantidad;
+  }
+  else(carrito.push(elementoAagregar))
+}
+
+document.addEventListener("click", (event) => {
+  if(event.target.classList.contains("Sumar_Item")){
+    const productoClickeado = event.target.closest("tr").dataset.id; 
+    const elemento = carrito.find(auto => auto.id == productoClickeado)
+    if(elemento){
+      elemento.cantidad += 1
+    }
+  }
+  if(event.target.classList.contains("Restar_Item")){
+    const productoClickeado = event.target.closest("tr").dataset.id; 
+    const elementoArestar = carrito.find(auto => auto.id == productoClickeado)
+    if(elementoArestar.cantidad !== 1){
+        elementoArestar.cantidad -= 1
+    }
+    else{
+      const index = carrito.indexOf(elementoArestar);
+      carrito.splice(index , 1)
+    }
+    }
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    document.querySelector("#items").innerHTML = ""
+    actualizarCarrito(carrito)
+
+})
+
+fetch("autos/Autos.txt")
+  .then(respuesta => (respuesta.json()))
+  .then((datos)=> (document.addEventListener("click", (event) =>{
+    if(event.target.classList.contains("Agregar_Item")){
+      const idConsulta = event.target.closest("article").dataset.id;
+      const elemento = (datos).find(auto => auto.id == idConsulta)
+      const producto = filtroDatosProducto(elemento)
+
+      console.log(producto)
+      agregarAlCarrito(producto)
+
+       //Guardo el carrito en formato JSON dentro del local storage
+       localStorage.setItem("carrito",JSON.stringify(carrito));
+
+    }
+})))
+
+
+
+function limpiarCarrito(carrito){
+    carrito.length = 0
+}
+
+
+
+
+function actualizarCarrito(carrito){
+    Object.values(carrito).forEach(item => {
+        const htmlcarrito = `
+            <tr data-id="${item.id}">
+            <td>${item.marca}</td>
+            <td>${item.modelo}</td>
+            <td>${item.precio}</td>
+            <td><button class = "Sumar_Item" >Agregar</button>${item.cantidad}<button class = "Restar_Item">Quitar</button></td>    
+        `
+        const items = document.querySelector("#items")
+        items.innerHTML += htmlcarrito;
+});
 
 
 }
 
+addEventListener("DOMContentLoaded",actualizarCarrito(carrito))
 
-document.addEventListener("DOMContentLoaded",traerDatosJSON("autos/Autos.txt"));
